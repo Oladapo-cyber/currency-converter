db234918a26afcd67036503e9a809d26ca1345a5
daps <ydapo50@gmail.com>
2025-01-09 10:56:39 +0100

    add currency conversion functionality and integrate axios for API calls

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import Inputs from "./Input";
 import { CgArrowsExchangeAltV } from "react-icons/cg";
+import axios from "axios";
 
 const Converter = () => {
+  const [currency, setCurrency] = useState("usd");
+  const [convertedCurrency, setConvertedCurrency] = useState("ngn");
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const handleConvert = async () => {
+    const res = await axios.get(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    );
+
+    console.log(res.data[currency][convertedCurrency] * amount);
+    setConvertedAmount(res.data[currency][convertedCurrency] * amount);
+  };
   return (
     <div className="bg-white h-[20rem] flex flex-col gap-5 justify-center items-center rounded-xl">
-      <Inputs />
+      <Inputs
+        amount={amount}
+        currency={currency}
+        setAmount={setAmount}
+        setCurrency={setCurrency}
+      />
       <CgArrowsExchangeAltV className="text-5xl text-black" />
-      <Inputs />
+      <Inputs
+        amount={convertedAmount}
+        setAmount={setConvertedAmount}
+        setCurrency={setConvertedCurrency}
+        currency={convertedCurrency}
+      />
 
-      <button className="rounded-lg px-5 font-semibold py-2 text-[#163300] bg-[#9fe870] hover:bg-[#8cd360] transition-all duration-300 ease-in-out">
+      <button
+        onClick={handleConvert}
+        className="rounded-lg px-5 font-semibold py-2 text-[#163300] bg-[#9fe870] hover:bg-[#8cd360] transition-all duration-300 ease-in-out"
+      >
         Convert
       </button>
     </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,32 @@
-const Inputs = () => {
+const Inputs = ({ amount, setAmount, setCurrency, currency }) => {
+  const currencyList = [
+    "ngn", // Nigeria
+    "ghs", // Ghana
+    "usd", // United States
+    "eur", // European Union
+    "gbp", // United Kingdom
+    "jpy", // Japan
+    "aud", // Australia
+    "cad", // Canada
+    "zar", // South Africa
+    "kes", //Kenya
+    "btc", //Bitcoin
+    "eth", //Ethereum
+  ];
   return (
     <span className=" w-fit relative px-2">
       <input
         className="border border-black w-[24rem] p-5 font-semibold text-black text-2xl rounded-lg"
         placeholder="0"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
       />
 
-      <select className="absolute right-5 bg-transparent p-5 font-semibold text-black text-2xl rounded-lg">
+      <select
+        onChange={(e) => setCurrency(e.target.value)}
+        value={currency}
+        className="absolute right-5 bg-transparent p-5 font-semibold text-black text-2xl rounded-lg"
+      >
         <option value="NGN">NGN</option>
         <option value="USD">USD</option>
       </select>
